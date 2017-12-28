c2d00e510e1e8e707a6d4605077bcc6d5d297289
Ken Wingerden <ken.wingerden@gmail.com>
2017-12-27 21:17:19 -0800

    Initial Commit

---
+++ ecodatum-ios-clientTests/ecodatum_ios_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  ecodatum_ios_clientTests.swift
+//  ecodatum-ios-clientTests
+//
+//  Created by Kenneth Wingerden on 12/27/17.
+//  Copyright © 2017 Ken Wingerden. All rights reserved.
+//
+
+import XCTest
+@testable import ecodatum_ios_client
+
+class ecodatum_ios_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
