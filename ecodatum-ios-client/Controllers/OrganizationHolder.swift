e02d29f93e8c61a22b8d87f624ca19bced3ac429
Kenneth Wingerden <ken.wingerden@gmail.com>
2018-01-23 23:38:05 -0800

    more refactoring and reduction of code

---
+++ ecodatum-ios-client/Controllers/OrganizationHolder.swift
@@ -0,0 +1,9 @@
+//
+//  OrganizationHolder.swift
+//  ecodatum-ios-client
+//
+//  Created by Kenneth Wingerden on 1/23/18.
+//  Copyright © 2018 Ken Wingerden. All rights reserved.
+//
+
+import Foundation
---
+++ ecodatum-ios-client/Controllers/TopNavigationChoiceNavigationController.swift
@@ -0,0 +1,9 @@
+//
+//  TopNavigationChoiceNavigationController.swift
+//  ecodatum-ios-client
+//
+//  Created by Kenneth Wingerden on 1/23/18.
+//  Copyright © 2018 Ken Wingerden. All rights reserved.
+//
+
+import Foundation
