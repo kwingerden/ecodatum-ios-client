fe90ca56a4be8c229c035fd4af597e1af9fb3863
Ken Wingerden <ken.wingerden@gmail.com>
2018-04-29 21:45:04 -0700

    continued refactor of data page

--- ecodatum-ios-client/Controllers/NewOrUpdateDataViewController.swift
+++ ecodatum-ios-client/Controllers/NewOrUpdateDataViewController.swift
@@ -44,7 +44,7 @@ fileprivate enum BioticFactor: String {
 
 fileprivate enum AirDataType: String {
 
-  case Anemometer
+  case Velocity
   case CarbonDioxide = "Carbon Dioxide"
   case Light
   case PAR
@@ -53,13 +53,13 @@ fileprivate enum AirDataType: String {
   case UVB
 
   static let all: [AirDataType] = [
-    .Anemometer,
+    .Velocity,
     .CarbonDioxide,
     .Light,
     .PAR,
     .RelativeHumidity,
     .Temperature,
-    .UVB
+    .UVB,
   ]
 
 }
@@ -143,17 +143,55 @@ fileprivate enum DataTypeChoice {
 
 }
 
+fileprivate enum DataUnitChoice: String {
+
+  case ppm
+  case Lux
+  case PPFD
+  case MMMS = "µmol m^-2 s^-1"
+  case Percent = "%"
+  case DegreesCelsius = "°C"
+  case DegreesFahrenheit = "°F"
+  case MWM = "mW/m^2"
+  case MPS = "m/s"
+  case mph
+  case LBA = "lb/a"
+  case MSCM = "µS/cm"
+  case FPS = "ft/s"
+  case MPL = "mg/L"
+  case NTU
+  case JTU
+  case Scale
+
+  static func units(_ dataType: DataTypeChoice) -> [DataUnitChoice] {
+
+    switch dataType {
+
+      case .Air(let airDataType) where airDataType == .Velocity:
+        return [.MPS, .mph]
+
+      default: return []
+
+    }
+
+  }
+
+}
+
 fileprivate struct AbioticFactorChoices {
 
   let abioticFactor: AbioticFactor?
   let dataType: DataTypeChoice?
+  let dataUnit: DataUnitChoice?
   let dataValue: Double?
 
   init(abioticFactor: AbioticFactor? = nil,
        dataType: DataTypeChoice? = nil,
+       dataUnit: DataUnitChoice? = nil,
        dataValue: Double? = nil) {
     self.abioticFactor = abioticFactor
     self.dataType = dataType
+    self.dataUnit = dataUnit
     self.dataValue = dataValue
   }
 
@@ -271,11 +309,18 @@ class NewOrUpdateDataViewController: BaseFormSheetDisplayable {
           destination.handleDataTypeChoice = handleDataTypeChoice
         }
 
-      case "dataValueChoice":
-        if let destination = segue.destination as? DataTypeChoiceViewController,
+      case "dataUnitChoice":
+        if let destination = segue.destination as? DataUnitChoiceViewController,
            let abioticFactorChoices = abioticFactorChoices {
           destination.abioticFactorChoices = abioticFactorChoices
-          destination.handleDataTypeChoice = handleDataTypeChoice
+          destination.handleDataUnitChoice = handleDataUnitChoice
+        }
+
+      case "dataValueChoice":
+        if let destination = segue.destination as? DataValueChoiceViewController,
+           let abioticFactorChoices = abioticFactorChoices {
+          destination.abioticFactorChoices = abioticFactorChoices
+          destination.handleDataValueChoice = handleDataValueChoice
         }
 
       default:
@@ -399,6 +444,38 @@ class NewOrUpdateDataViewController: BaseFormSheetDisplayable {
 
   }
 
+  func presentDataUnitChoice() {
+    performSegue(withIdentifier: "dataUnitChoice", sender: nil)
+  }
+
+  fileprivate func handleDataUnitChoice(_ dataUnitChoice: DataUnitChoice) {
+
+    if let abioticFactorChoices = abioticFactorChoices,
+       let dataUnit = abioticFactorChoices.dataUnit,
+       dataUnit == dataUnitChoice {
+      return
+    }
+
+    if let abioticFactorChoices = abioticFactorChoices {
+      self.abioticFactorChoices = AbioticFactorChoices(
+        abioticFactor: abioticFactorChoices.abioticFactor,
+        dataType: abioticFactorChoices.dataType,
+        dataUnit: dataUnitChoice)
+    }
+    tableView.reloadSections([5], with: .automatic)
+
+    if numberOfSections > 6 {
+      for sectionIndex in (7...numberOfSections).reversed() {
+        numberOfSections = sectionIndex - 1
+        tableView.deleteSections([numberOfSections], with: .automatic)
+      }
+    }
+
+    numberOfSections = 7
+    tableView.insertSections([6], with: .automatic)
+
+  }
+
   func presentDataValueChoice() {
     performSegue(withIdentifier: "dataValueChoice", sender: nil)
   }
@@ -417,6 +494,14 @@ class NewOrUpdateDataViewController: BaseFormSheetDisplayable {
         dataValue: dataValueChoice)
     }
     tableView.reloadSections([5], with: .automatic)
+
+    if numberOfSections > 6 {
+      for sectionIndex in (7...numberOfSections).reversed() {
+        numberOfSections = sectionIndex - 1
+        tableView.deleteSections([numberOfSections], with: .automatic)
+      }
+    }
+
     numberOfSections = 7
     tableView.insertSections([6], with: .automatic)
 
@@ -463,6 +548,16 @@ extension NewOrUpdateDataViewController: UITableViewDataSource {
       if let abioticFactorChoices = abioticFactorChoices,
          let abioticFactor = abioticFactorChoices.abioticFactor {
         switch abioticFactor {
+        case .Air: return "Air Data Unit"
+        case .Soil: return "Soil Data Unit"
+        case .Water: return "Water Data Unit"
+        }
+      }
+      return nil
+    case 6:
+      if let abioticFactorChoices = abioticFactorChoices,
+         let abioticFactor = abioticFactorChoices.abioticFactor {
+        switch abioticFactor {
         case .Air: return "Air Data Value"
         case .Soil: return "Soil Data Value"
         case .Water: return "Water Data Value"
@@ -488,7 +583,8 @@ extension NewOrUpdateDataViewController: UITableViewDataSource {
     case 2: return makeEcoFactorChoiceCell(tableView, indexPath)
     case 3: return makeAbioticFactorChoiceCell(tableView, indexPath)
     case 4: return makeDataTypeChoiceCell(tableView, indexPath)
-    case 5: return makeDataValueChoiceCell(tableView, indexPath)
+    case 5: return makeDataUnitChoiceCell(tableView, indexPath)
+    case 6: return makeDataValueChoiceCell(tableView, indexPath)
     default: return UITableViewCell()
 
     }
@@ -619,6 +715,41 @@ extension NewOrUpdateDataViewController: UITableViewDataSource {
     }
 
     cell.presentDataTypeChoice = presentDataTypeChoice
+
+    return cell
+
+  }
+
+  private func makeDataUnitChoiceCell(
+    _ tableView: UITableView,
+    _ indexPath: IndexPath) -> UITableViewCell {
+
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: "dataUnitChoice",
+      for: indexPath) as! DataUnitChoiceTableViewCell
+
+    if let abioticFactorChoices = abioticFactorChoices,
+       let dataUnit = abioticFactorChoices.dataUnit {
+
+      cell.dataUnitLabel.text = dataUnit.rawValue
+      cell.dataUnitLabel.textColor = .black
+
+    } else {
+
+      if let abioticFactorChoices = abioticFactorChoices,
+         let abioticFactor = abioticFactorChoices.abioticFactor {
+        switch abioticFactor {
+        case .Air: cell.dataUnitLabel.text = "Choose Air Data Unit"
+        case .Soil: cell.dataUnitLabel.text = "Choose Soil Data Unit"
+        case .Water: cell.dataUnitLabel.text = "Choose Water Data Unit"
+        }
+      }
+
+      cell.dataUnitLabel.textColor = .lightGray
+
+    }
+
+    cell.presentDataUnitChoice = presentDataUnitChoice
 
     return cell
 
@@ -949,6 +1080,73 @@ class DataTypeChoiceViewController: UIViewController, UIPickerViewDataSource, UI
 
 }
 
+class DataUnitChoiceTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var dataUnitLabel: UILabel!
+
+  var presentDataUnitChoice: (() -> Void)!
+
+  @IBAction func touchUpInside() {
+    presentDataUnitChoice()
+  }
+
+}
+
+class DataUnitChoiceViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+  @IBOutlet weak var dataUnitPicker: UIPickerView!
+
+  fileprivate var abioticFactorChoices: AbioticFactorChoices!
+
+  fileprivate var handleDataUnitChoice: ((DataUnitChoice) -> Void)!
+
+  private var dataUnits: [DataUnitChoice] = []
+
+  override func viewDidLoad() {
+
+    super.viewDidLoad()
+
+    dataUnitPicker.dataSource = self
+    dataUnitPicker.delegate = self
+
+    if let dataType = abioticFactorChoices.dataType {
+      dataUnits = DataUnitChoice.units(dataType)
+    }
+
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    var selectedRow: Int = 0
+    if let dataUnitChoice = abioticFactorChoices.dataUnit {
+      selectedRow = dataUnits.index(of: dataUnitChoice)!
+    }
+
+    dataUnitPicker.selectRow(selectedRow, inComponent: 0, animated: false)
+
+  }
+
+  @IBAction func touchUpInside() {
+    let selectedRow = dataUnitPicker.selectedRow(inComponent: 0)
+    handleDataUnitChoice(dataUnits[selectedRow])
+    dismiss(animated: true, completion: nil)
+  }
+
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return dataUnits.count
+  }
+
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return dataUnits[row].rawValue
+  }
+
+}
+
 class DataValueChoiceTableViewCell: UITableViewCell {
 
   @IBOutlet weak var dataValueLabel: UILabel!
@@ -963,6 +1161,40 @@ class DataValueChoiceTableViewCell: UITableViewCell {
 
 class DataValueChoiceViewController: UIViewController {
 
+  @IBOutlet weak var valueLabel: UILabel!
+  
+  @IBOutlet weak var clearButton: UIButton!
+  
+  @IBOutlet weak var signButton: UIButton!
+  
+  @IBOutlet weak var deleteButton: UIButton!
+  
+  @IBOutlet weak var zeroButton: UIButton!
+  
+  @IBOutlet weak var oneButton: UIButton!
+  
+  @IBOutlet weak var twoButton: UIButton!
+  
+  @IBOutlet weak var threeButton: UIButton!
+  
+  @IBOutlet weak var fourButton: UIButton!
+  
+  @IBOutlet weak var fiveButton: UIButton!
+  
+  @IBOutlet weak var sixButton: UIButton!
+  
+  @IBOutlet weak var sevenButton: UIButton!
+  
+  @IBOutlet weak var eightButton: UIButton!
+  
+  @IBOutlet weak var nineButton: UIButton!
+  
+  @IBOutlet weak var decimalButton: UIButton!
+  
+  @IBOutlet weak var okButton: UIButton!
+  
+  private var value: String = "0"
+  
   fileprivate var abioticFactorChoices: AbioticFactorChoices!
 
   fileprivate var handleDataValueChoice: ((Double) -> Void)!
@@ -970,9 +1202,151 @@ class DataValueChoiceViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    valueLabel.roundedAndDarkBordered()
+    
+    clearButton.roundedAndDarkBordered()
+    signButton.roundedAndDarkBordered()
+    deleteButton.roundedAndDarkBordered()
+    decimalButton.roundedAndDarkBordered()
+    okButton.roundedAndDarkBordered()
+    
+    zeroButton.roundedAndDarkBordered()
+    oneButton.roundedAndDarkBordered()
+    twoButton.roundedAndDarkBordered()
+    threeButton.roundedAndDarkBordered()
+    fourButton.roundedAndDarkBordered()
+    fiveButton.roundedAndDarkBordered()
+    sixButton.roundedAndDarkBordered()
+    sevenButton.roundedAndDarkBordered()
+    eightButton.roundedAndDarkBordered()
+    nineButton.roundedAndDarkBordered()
+
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    if let dataValue = abioticFactorChoices.dataValue {
+      value = String(dataValue)
+      valueLabel.text = "\(value)"
+    }
+
+  }
+
+  @IBAction func touchUpInside(_ sender: UIButton) {
+
+    var newValue = value
+
+    switch sender {
+      
+    case clearButton:
+      newValue = "0"
+      
+    case signButton:
+      let doubleValue = Double(newValue)!
+      if doubleValue != 0.0 {
+        newValue = "\(-Double(newValue)!)"
+      }
+      
+    case deleteButton:
+      if newValue.count > 0 {
+        let beforeEndIndex = newValue.index(before: newValue.endIndex)
+        newValue = String(value[..<beforeEndIndex])
+        if newValue.isEmpty {
+          newValue = "0"
+        }
+      }
+      
+    case decimalButton:
+      if !newValue.contains(".") {
+        newValue = newValue + "."
+      }
+      
+    case zeroButton:
+      if newValue == "0" {
+        return
+      } else {
+        newValue = newValue + "0"
+      }
+      
+    case oneButton:
+      if newValue == "0" {
+        newValue = "1"
+      } else {
+        newValue = newValue + "1"
+      }
+      
+    case twoButton:
+      if newValue == "0" {
+        newValue = "2"
+      } else {
+        newValue = newValue + "2"
+      }
+      
+    case threeButton:
+      if newValue == "0" {
+        newValue = "3"
+      } else {
+        newValue = newValue + "3"
+      }
+      
+    case fourButton:
+      if newValue == "0" {
+        newValue = "4"
+      } else {
+        newValue = newValue + "4"
+      }
+      
+    case fiveButton:
+      if newValue == "0" {
+        newValue = "5"
+      } else {
+        newValue = newValue + "5"
+      }
+      
+    case sixButton:
+      if newValue == "0" {
+        newValue = "6"
+      } else {
+        newValue = newValue + "6"
+      }
+      
+    case sevenButton:
+      if newValue == "0" {
+        newValue = "7"
+      } else {
+        newValue = newValue + "7"
+      }
+      
+    case eightButton:
+      if newValue == "0" {
+        newValue = "8"
+      } else {
+        newValue = newValue + "8"
+      }
+      
+    case nineButton:
+      if newValue == "0" {
+        newValue = "9"
+      } else {
+        newValue = newValue + "9"
+      }
+      
+    case okButton:
+      handleDataValueChoice(Double(value)!)
+      dismiss(animated: true, completion: nil)
+      
+    default:
+      LOG.error("Unexpected button \(sender)")
+      
+    }
+
+    if newValue.count <= 10 {
+      value = newValue
+      valueLabel.text = "\(value)"
+    }
+    
   }
 
 }
-
-
 
